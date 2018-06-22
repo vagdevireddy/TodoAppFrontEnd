53222318f58cdaa9efb095eaba68201c453e306c
ravi gouni <gouniravi@gmail.com>
2018-06-22 11:43:55 -0500

    2 commit

--- public/core.js
+++ public/core.js
@@ -5,7 +5,7 @@ function mainController($scope, $http) {
     $scope.formData = {};
 
     // when landing on the page, get all todos and show them
-    $http.get('/api/todos')
+    $http.get('http://35.161.3.197:8080/api/todos')
         .success(function(data) {
             $scope.todos = data;
             console.log(data);
@@ -16,7 +16,7 @@ function mainController($scope, $http) {
 
     // when submitting the add form, send the text to the node API
     $scope.createTodo = function() {
-        $http.post('/api/todos', $scope.formData)
+        $http.post('http://35.161.3.197:8080/api/todos', $scope.formData)
             .success(function(data) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
                 $scope.todos = data;
@@ -29,7 +29,7 @@ function mainController($scope, $http) {
 
     // delete a todo after checking it
     $scope.deleteTodo = function(id) {
-        $http.delete('/api/todos/' + id)
+        $http.delete('http://35.161.3.197:8080/api/todos/' + id)
             .success(function(data) {
                 $scope.todos = data;
                 console.log(data);
