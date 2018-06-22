f0719f1a0454c8c2378b88031c979270fb185a36
ravi gouni <gouniravi@gmail.com>
2018-06-22 11:48:19 -0500

    2 commit

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // server.js
-
+    
+	// set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+	
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
